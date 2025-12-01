e7f1032df63905db24075d059737cfee3d0350d1
Felipe villa <andresfelipevillapardo7@gmail.com>
2025-12-01 12:27:30 -0500

    Refactor: corrección de errores y limpieza del código, eliminando importaciones innecesarias.

---
+++ mi_informe_guaviare/src/main/java/com/informeguaviare/mi_informe_guaviare/application/port/in/LoginUseCase.java
@@ -0,0 +1,4 @@
+package com.informeguaviare.mi_informe_guaviare.application.port.in;
+
+public interface LoginUseCase {
+}
---
+++ mi_informe_guaviare/src/main/java/com/informeguaviare/mi_informe_guaviare/application/service/LoginService.java
@@ -0,0 +1,4 @@
+package com.informeguaviare.mi_informe_guaviare.application.service;
+
+public class LoginService {
+}
